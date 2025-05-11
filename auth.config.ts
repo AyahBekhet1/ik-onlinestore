4e0ca583c5dd924a6f1bea4763248b7bf6813bc9
AyahBekhet1 <aya.bekhet4794@gmail.com>
2025-05-11 16:45:21 +0300

    nighth commit

---
+++ auth.config.ts
@@ -0,0 +1,47 @@
+import type { NextAuthConfig } from 'next-auth'
+import { NextResponse } from 'next/server'
+
+
+export const authConfig ={
+    providers:[],
+    callbacks:{
+        authorized({request, auth}:any){
+            //array of regex patterns of paths we want to protect
+            const protectedPaths = [
+                /\/profile/,
+                /\/user\/(.*)/,
+                /\/order\/(.*)/,
+                /\/admin/,
+            ]
+
+            //get pathname from the req url object
+            const {pathname}=request.nextUrl
+        
+
+            //check if user not authenticated and accessing a protecting path
+            //!auth tell us the user is a guest not logged in
+            if(!auth && protectedPaths.some((p)=>p.test(pathname))) return false
+
+            // check for session cart cookie
+            if(!request.cookies.get('sessionCartId')){
+                //generate new session cart id cookie
+                const sessionCartId=crypto.randomUUID()
+                //clone the req headers
+                const newRequestHeaders = new Headers(request.headers)
+
+                //create new response and add the new headers
+                const response = NextResponse.next({
+                    request:{
+                        headers:newRequestHeaders
+                    }
+                });
+                //set newly generated sessionCartId in the response cookies
+                response.cookies.set('sessionCartId', sessionCartId)
+                // Return the response with the sessionCartId set
+                return response
+            }else{
+                return true
+            }
+          }
+    }
+}satisfies NextAuthConfig
--- auth.ts
+++ auth.ts
@@ -1,6 +1,5 @@
 import { cookies } from 'next/headers';
 import NextAuth from 'next-auth'
-import type { NextAuthConfig } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 
 import {PrismaAdapter} from '@auth/prisma-adapter'
@@ -9,7 +8,7 @@ import { prisma } from './db/prisma'
 import { compareSync } from 'bcrypt-ts-edge'
 import GoogleProvider from "next-auth/providers/google"
 
-import { NextResponse } from 'next/server'
+import { authConfig } from './auth.config';
 
 export const config ={
     pages:{
@@ -17,7 +16,7 @@ export const config ={
         error:'/sign-in',
     },
     session :{
-        strategy:'jwt',
+        strategy:'jwt' as const,
         maxAge: 60 * 24 * 60 * 60,
 
     },
@@ -72,6 +71,7 @@ export const config ={
     // secret: process.env.NEXTAUTH_SECRET,
 
     callbacks:{
+        ...authConfig.callbacks,
         async session({ session , user,trigger , token } : any) {
             //set the user id from the token
             session.user.id= token.sub;
@@ -139,45 +139,8 @@ export const config ={
             }
             return true // Do different verification for other providers that don't have `email_verified`
           },
-          authorized({request, auth}:any){
-            //array of regex patterns of paths we want to protect
-            const protectedPaths = [
-                /\/profile/,
-                /\/user\/(.*)/,
-                /\/order\/(.*)/,
-                /\/admin/,
-            ]
-
-            //get pathname from the req url object
-            const {pathname}=request.nextUrl
         
-
-            //check if user not authenticated and accessing a protecting path
-            //!auth tell us the user is a guest not logged in
-            if(!auth && protectedPaths.some((p)=>p.test(pathname))) return false
-
-            // check for session cart cookie
-            if(!request.cookies.get('sessionCartId')){
-                //generate new session cart id cookie
-                const sessionCartId=crypto.randomUUID()
-                //clone the req headers
-                const newRequestHeaders = new Headers(request.headers)
-
-                //create new response and add the new headers
-                const response = NextResponse.next({
-                    request:{
-                        headers:newRequestHeaders
-                    }
-                });
-                //set newly generated sessionCartId in the response cookies
-                response.cookies.set('sessionCartId', sessionCartId)
-                // Return the response with the sessionCartId set
-                return response
-            }else{
-                return true
-            }
-          }
     }
-} satisfies NextAuthConfig
+} 
 
 export const {handlers , auth , signIn , signOut} = NextAuth(config)
