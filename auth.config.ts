d4494ad8cd4d809040b109038eb0ffb010ac8a54
AyahBekhet1 <aya.bekhet4794@gmail.com>
2025-05-11 19:17:23 +0300

    fourteen commit

--- auth.config.ts
+++ auth.config.ts
@@ -1,11 +1,12 @@
 import type { NextAuthConfig } from 'next-auth'
 import { NextResponse } from 'next/server'
+import type { NextRequest } from "next/server";
 
 
 export const authConfig ={
     providers:[],
     callbacks:{
-        authorized({request, auth}:any){
+        authorized({request, auth}:{ request: NextRequest; auth: any }){
             //array of regex patterns of paths we want to protect
             const protectedPaths = [
                 /\/profile/,
@@ -25,7 +26,10 @@ export const authConfig ={
             // check for session cart cookie
             if(!request.cookies.get('sessionCartId')){
                 //generate new session cart id cookie
-                const sessionCartId=crypto.randomUUID()
+                const sessionCartId = Array.from(crypto.getRandomValues(new Uint8Array(16)))
+                .map((b) => b.toString(16).padStart(2, '0'))
+                .join('');
+              
                 //clone the req headers
                 const newRequestHeaders = new Headers(request.headers)
 
