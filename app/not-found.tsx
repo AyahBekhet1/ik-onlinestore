eef63097fd5f6e7b5b0472940e59bfccf08cbc2f
AyahBekhet1 <aya.bekhet4794@gmail.com>
2025-05-11 15:55:26 +0300

    fifth commit

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,20 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Player } from "@lottiefiles/react-lottie-player";
+import { useEffect, useState } from "react";
 
 export default function NotFound() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true); // This ensures we only run this code client-side
+  }, []);
+
+  const handleBackHome = () => {
+    if (isClient) {
+      window.location.href = "/";
+    }
+  };
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
        <div>
@@ -11,13 +23,12 @@ export default function NotFound() {
           loop
           className="not-found"
           src='/lottie/notFoundPage.json'
-          // style={{ height: "450px", width: "450px" }}
         />
       </div>
       <div className='p-6 rounded-lg flex justify-center items-center text-center'>
         <Button
           className='mt-4 ml-2'
-          onClick={() => (window.location.href = "/")}
+          onClick={handleBackHome}
         >
           Back Home
         </Button>
