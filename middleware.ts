4a3b4e8cb1faa36d2a292a4cd3a8d1f3f9ad139b
AyahBekhet1 <aya.bekhet4794@gmail.com>
2025-05-12 11:41:49 +0300

    twenty-six commit

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,21 @@
-// import NextAuth from 'next-auth';
-// import { authConfig } from './auth.config';
-
-// export const { auth: middleware } = NextAuth(authConfig);
-
-
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 
-export default NextAuth(authConfig);;
+export const { auth: middleware } = NextAuth(authConfig);
 
-export const config = {
-  matcher: [
-    '/shipping-address',
-    '/payment-method',
-    '/place-order',
-    '/profile',
-    '/user/:path*',
-    '/order/:path*',
-    '/admin/:path*',
-  ],
-};
+// import NextAuth from 'next-auth';
+// import { authConfig } from './auth.config';
+
+// export default NextAuth(authConfig);;
+
+// export const config = {
+//   matcher: [
+//     '/shipping-address',
+//     '/payment-method',
+//     '/place-order',
+//     '/profile',
+//     '/user/:path*',
+//     '/order/:path*',
+//     '/admin/:path*',
+//   ],
+// };
