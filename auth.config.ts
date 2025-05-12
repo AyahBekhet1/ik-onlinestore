a47c13876e75992251d88d62e5125750d3cd714e
AyahBekhet1 <aya.bekhet4794@gmail.com>
2025-05-12 11:11:09 +0300

    twenty-four commit

--- auth.config.ts
+++ auth.config.ts
@@ -9,43 +9,43 @@ export const authConfig = {
   callbacks: {
     authorized({ request, auth }: any) {
       // Array of regex patterns of paths we want to protect
-//       const protectedPaths = [
-//         /\/shipping-address/,
-//         /\/payment-method/,
-//         /\/place-order/,
-//         /\/profile/,
-//         /\/user\/(.*)/,
-//         /\/order\/(.*)/,
-//         /\/admin/,
-//       ];
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin/,
+      ];
 
-//       // Get pathname from the req URL object
-//       const { pathname } = request.nextUrl;
-//       // Check if user is not authenticated and accessing a protected path
-//       if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+      // Get pathname from the req URL object
+      const { pathname } = request.nextUrl;
+      // Check if user is not authenticated and accessing a protected path
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
 
-//       // Check for session cart cookie
-//       if (!request.cookies.get('sessionCartId')) {
-//         // Generate new session cart id cookie
-//         let sessionCartId = '';
-// if (typeof crypto !== 'undefined' && crypto.randomUUID) {
-//   sessionCartId = crypto.randomUUID();
-// } else {
-//   sessionCartId = Math.random().toString(36).substring(2, 15);
-// }
+      // Check for session cart cookie
+      if (!request.cookies.get('sessionCartId')) {
+        // Generate new session cart id cookie
+        let sessionCartId = '';
+if (typeof crypto !== 'undefined' && crypto.randomUUID) {
+  sessionCartId = crypto.randomUUID();
+} else {
+  sessionCartId = Math.random().toString(36).substring(2, 15);
+}
 
-//         // Create new response and add the new headers
-//         const response = NextResponse.next({
-//           request: {
-//             headers: new Headers(request.headers),
-//           },
-//         });
+        // Create new response and add the new headers
+        const response = NextResponse.next({
+          request: {
+            headers: new Headers(request.headers),
+          },
+        });
 
-//         // Set newly generated sessionCartId in the response cookies
-//         response.cookies.set('sessionCartId', sessionCartId);
+        // Set newly generated sessionCartId in the response cookies
+        response.cookies.set('sessionCartId', sessionCartId);
 
-//         return response;
-//       }
+        return response;
+      }
 
       return true;
     },
---
+++ middleware.ts
@@ -0,0 +1,24 @@
+// import NextAuth from 'next-auth';
+// import { authConfig } from './auth.config';
+
+// export const { auth: middleware } = NextAuth(authConfig);
+
+
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+
+export const { auth: middleware } = NextAuth(authConfig);
+
+export default middleware;
+
+export const config = {
+  matcher: [
+    '/shipping-address',
+    '/payment-method',
+    '/place-order',
+    '/profile',
+    '/user/:path*',
+    '/order/:path*',
+    '/admin/:path*',
+  ],
+};
